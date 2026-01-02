fb6dc95cad2909ab40e8e00fed4d743086b30e7d
Jan Mewes <jan.mewes@experimental-software.com>
2026-01-02 16:52:39 +0100

    chore: improve test coverage

--- logging/logging.go
+++ logging/logging.go
@@ -12,17 +12,17 @@ var (
 )
 
 func init() {
-	infoLog = log.New(os.Stdout, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
-	warningLog = log.New(os.Stderr, "WARNING: ", log.Ldate|log.Ltime|log.Lshortfile)
+	infoLog = log.New(os.Stdout, "", log.Ldate|log.Ltime|log.Lshortfile)
+	warningLog = log.New(os.Stderr, "", log.Ldate|log.Ltime|log.Lshortfile)
 	errorLog = log.New(os.Stderr, "", log.Ldate|log.Ltime|log.Lshortfile)
 }
 
 func Info(message string) {
-	infoLog.Println(message)
+	infoLog.Println("INFO: " + message)
 }
 
 func Warn(message string) {
-	warningLog.Println(message)
+	warningLog.Println("WARNING: " + message)
 }
 
 func Error(message string, err error) {
---
+++ logging/logging_test.go
@@ -0,0 +1,53 @@
+package logging
+
+import (
+	"bytes"
+	"errors"
+	"strings"
+	"testing"
+)
+
+func TestInfoLog(t *testing.T) {
+	// Arrange
+	var str bytes.Buffer
+	infoLog.SetOutput(&str)
+
+	// Act
+	Info("For your information")
+
+	// Assert
+	output := strings.TrimSuffix(str.String(), "\n")
+	if !(strings.HasSuffix(output, "INFO: For your information")) {
+		t.Errorf("Unexpected info log output: %s", str.String())
+	}
+}
+
+func TestWarningLog(t *testing.T) {
+	// Arrange
+	var str bytes.Buffer
+	warningLog.SetOutput(&str)
+
+	// Act
+	Warn("System is in unexpected state")
+
+	// Assert
+	output := strings.TrimSuffix(str.String(), "\n")
+	if !(strings.HasSuffix(output, "WARNING: System is in unexpected state")) {
+		t.Errorf("Unexpected warning log output: %s", str.String())
+	}
+}
+
+func TestErrorLog(t *testing.T) {
+	// Arrange
+	var str bytes.Buffer
+	errorLog.SetOutput(&str)
+
+	// Act
+	Error("something went wrong", errors.New("this is an error"))
+
+	// Assert
+	output := strings.TrimSuffix(str.String(), "\n")
+	if !(strings.Contains(output, "ERROR: something went wrong")) {
+		t.Errorf("Unexpected error log output: %s", str.String())
+	}
+}
