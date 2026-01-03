d46ad8f8089aa4788ba47afb053e966f654df87f
Jan Mewes <jan.mewes@experimental-software.com>
2026-01-03 09:14:18 +0100

    feat: support multiple path for archive

--- cmd/archive.go
+++ cmd/archive.go
@@ -10,16 +10,19 @@ import (
 )
 
 var archiveCmd = &cobra.Command{
-	Use:  "archive [flags] path",
-	Args: cobra.ExactArgs(1),
+	Use:     "archive [flags] path [path...]",
+	Short:   "Moves log entries into the archive directory",
+	Args:    cobra.MinimumNArgs(1),
+	Aliases: []string{"a"},
 
 	Run: func(cmd *cobra.Command, args []string) {
-		path := args[0]
-		err := core.Archive(configuration, path)
-		if err != nil {
-			logging.Error("Archive failed", err)
-			os.Exit(1)
+		for _, path := range args {
+			err := core.Archive(configuration, path)
+			if err != nil {
+				logging.Error("Archive failed", err)
+				os.Exit(1)
+			}
+			fmt.Println(path)
 		}
-		fmt.Println(path)
 	},
 }
