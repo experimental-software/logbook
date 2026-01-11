41423b7f51821a3f23657277027d612de330deda
Jan Mewes <jan.mewes@experimental-software.com>
2026-01-11 17:48:24 +0100

    Move function on other file

--- core/deleting.go
+++ core/deleting.go
@@ -1,10 +1,7 @@
 package core
 
 import (
-	"errors"
 	"os"
-	"regexp"
-	"strings"
 )
 
 func Remove(sourcePath string) error {
@@ -14,16 +11,4 @@ func Remove(sourcePath string) error {
 	}
 	err = os.RemoveAll(sourceDirectoryPath)
 	return err
-}
-
-func logbookEntryRootPath(path string) (string, error) {
-	if !strings.HasSuffix(path, "/") {
-		path += "/"
-	}
-	re := regexp.MustCompile(`(.*[/\\]\d{4}[/\\]\d{2}[/\\]\d{2}[/\\]\d{2}\.\d{2}_.*?[/\\]).*`)
-	m := re.FindStringSubmatch(path)
-	if len(m) != 2 {
-		return "", errors.New("invalid logbook entry path: " + path)
-	}
-	return m[1], nil
 }
