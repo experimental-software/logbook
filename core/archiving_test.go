fd9bdd100b47f522e23bcfc49f78570191df5559
Jan Mewes <jan.mewes@experimental-software.com>
2026-01-11 17:46:49 +0100

    chore: split out management functions in multiple files

--- core/archiving_test.go
+++ core/archiving_test.go
@@ -1,45 +1,12 @@
 package core
 
 import (
-	"fmt"
 	"os"
-	"path/filepath"
 	"testing"
 	"time"
 
 	"github.com/experimental-software/logbook2/config"
 )
-
-func Test_Delete_happy_path(t *testing.T) {
-	// Arrange
-	logBaseDir := createTempDir()
-	archiveBaseDir := createTempDir()
-	defer func(path string) {
-		_ = os.RemoveAll(logBaseDir)
-		_ = os.RemoveAll(archiveBaseDir)
-	}(logBaseDir)
-
-	logEntry, err := AddLogEntry(logBaseDir, "Log entry for archive test", time.Now())
-	if err != nil {
-		t.Fatal(err)
-	}
-	searchResultForArchiveBaseDir := Search(logBaseDir, "", epoc, nextCentury)
-	if len(searchResultForArchiveBaseDir) != 1 {
-		t.Fatal("Expected 1 search result")
-	}
-
-	// Act
-	err = Remove(logEntry.Directory)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	// Assert
-	searchResultForLogBaseDir := Search(logBaseDir, "", epoc, nextCentury)
-	if len(searchResultForLogBaseDir) != 0 {
-		t.Fatal("Expected empty search result")
-	}
-}
 
 func Test_Archive_happy_path(t *testing.T) {
 	// Arrange
@@ -112,27 +79,4 @@ func Test_Archive_path_in_subdirectory(t *testing.T) {
 	if len(searchResultForArchiveBaseDir) != 1 {
 		t.Fatal("Expected 1 search result")
 	}
-}
-
-func Test_logbookEntryRootPath_invalid_path(t *testing.T) {
-	_, err := logbookEntryRootPath("/Users/jdoe/Notes/2025/12/21/just-a-test")
-	if err == nil {
-		t.Fatal("Expected error")
-	}
-	fmt.Println(err.Error())
-}
-
-func createFileInSubdirectory(entry LogbookEntry) string {
-	subdirectory := filepath.Join(entry.Directory, "foo")
-	err := os.MkdirAll(subdirectory, 0755)
-	if err != nil {
-		panic(err)
-	}
-	filePath := filepath.Join(subdirectory, "bar.txt")
-	data := []byte("Hello, World!")
-	err = os.WriteFile(filePath, data, 0644)
-	if err != nil {
-		panic(err)
-	}
-	return filePath
 }
